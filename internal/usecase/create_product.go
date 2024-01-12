4dcded2b865ddaf0efff57640a188d36850f736b
Laycon John <laycon.gomes@tld.local>
2024-01-12 13:24:48 -0300

    feat: CreatePrductUseCase

--- internal/usecase/create_product.go
+++ internal/usecase/create_product.go
@@ -17,4 +17,15 @@ type CreateProductUseCase struct {
 	ProductRepository entity.ProductRepository
 }
 
-func (u *CreateProductUseCase) Execute(input CreateProductInputDto)
+func (u *CreateProductUseCase) Execute(input CreateProductInputDto) (*CreateProductOutputDto, error) {
+	product := entity.NewProduct(input.Name, input.Price)
+	err := u.ProductRepository.Create(product)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateProductOutputDto{
+		ID:    product.ID,
+		Name:  product.Name,
+		Price: product.Price,
+	}, nil
+}
